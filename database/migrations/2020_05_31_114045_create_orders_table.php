289b4f4d64c88d4e61a5ab5a5b4b9780ce48f00a
m.poliit <maks.poliit@gmail.com>
2020-06-01 20:01:33 +0300

    add migrations

---
+++ database/migrations/2020_05_31_114045_create_orders_table.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('user_name', 55);
+            $table->string('user_surname', 55);
+            $table->string('user_email');
+            $table->string('user_phone');
+            $table->string('country');
+            $table->string('city');
+            $table->string('address');
+            $table->float('total');
+            $table->unsignedBigInteger('status_id');
+            $table->string('type')->unique();
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
+
+            $table->foreign('status_id')
+                ->references('id')
+                ->on('orders_statuses');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('orders', function (Blueprint $table){
+            if(Schema::hasColumn('orders', 'user_id')) {
+                $table->dropForeign(['user_id']);
+            }
+            if(Schema::hasColumn('orders', 'status_id')) {
+                $table->dropForeign(['status_id']);
+            }
+        });
+        Schema::dropIfExists('orders');
+    }
+}
