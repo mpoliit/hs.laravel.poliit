a08fdd76287877ad9c9915785f29d89726e2a4c9
m.poliit <maks.poliit@gmail.com>
2020-07-01 18:52:09 +0300

    add ajax

---
+++ resources/views/admin/products/edit.blade.php
@@ -0,0 +1,90 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Create Category</div>
+
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('admin.categories.update', $category) }}" enctype="multipart/form-data">
+                            @csrf
+                            @method('PUT')
+
+                            <div class="form-group row">
+                                <label for="title" class="col-md-4 col-form-label text-md-right">{{ __('Title') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="title"
+                                           type="text"
+                                           class="form-control @error('title') is-invalid @enderror"
+                                           name="title"
+                                           value="{{ $category->title ?? '' }}"
+                                           required
+                                           autofocus>
+
+                                    @error('title')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row" style="display: flex; flex-direction: column; align-items: center; justify-content: center;">
+
+                                @if(!empty($image))
+                                    <img src="{{ Storage::disk('public')->url($image['path']) }}" height="250" width="250" />
+                                @endif
+
+                                <label for="image" class="col-md-4 col-form-label text-md-right">{{ __('Image') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="image"
+                                           type="file"
+                                           class="form-control @error('image') is-invalid @enderror"
+                                           name="image"
+                                           value="{{ old('image') }}">
+
+                                    @error('image')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="description" class="col-md-4 col-form-label text-md-right">{{ __('Description') }}</label>
+
+                                <div class="col-md-6">
+                                    <textarea name="description"
+                                              id="description"
+                                              cols="30"
+                                              rows="10"
+                                              class="form-control @error('description') is-invalid @enderror"
+                                    >{{ $category->description ?? '' }}</textarea>
+                                    @error('description')
+                                    <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+                    </div>
+
+                            <div class="form-group row mb-0">
+                                <div class="col-md-8 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        {{ __('Update') }}
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/admin/products/index.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-sm-12">
+                <table class="table table-striped">
+                    <thead>
+                    <tr>
+                        <th scope="col">{{__('Id')}}</th>
+                        <th scope="col">{{__('Title')}}</th>
+                        <th scope="col">{{__('Description')}}</th>
+                        <th scope="col" class="text-center">{{__('Action')}}</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        @each('admin.categories.parts.category_row', $categories, 'category')
+                    </tbody>
+                </table>
+
+                {{ $categories->links() }}
+            </div>
+        </div>
+    </div>
+    </div>
+@endsection
